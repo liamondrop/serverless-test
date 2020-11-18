5ebc9514fe9ae92e874eeeec1d5cfd2d4f0f8d29
Liam Bowers <liamondrop@gmail.com>
2020-11-18 10:57:10 -0800

    setting up react app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notesuploadtest",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2zf8xngauj.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_fc7GVO2hV",
+    APP_CLIENT_ID: "13bmd1t79qiom9qeooln1rua58",
+    IDENTITY_POOL_ID: "us-west-2:04de2636-ca7f-45a3-879e-090fdfd02a1a",
+  },
+};
+
+export default config;
